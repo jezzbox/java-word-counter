b1b0760127df4b198ab136d6c729ac98616b4b0e
Jeremy <jeremy.keelty@queryclick.com>
2023-02-23 18:40:56 +0000

    remove unused imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
